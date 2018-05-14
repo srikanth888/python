f47d49241c268c434be261192b0dd04ed7f792b4
srikanth888 <39044433+srikanth888@users.noreply.github.com>
2018-05-14 08:28:47 +0530

    nested dict

---
+++ dict3.py
@@ -0,0 +1,22 @@
+dict1 = {
+    1234 : {
+        'fname' : "srikanth",
+        'lname' : "kandi",
+        'mname' : "reddy",
+        'uid' : 12345678910
+    },
+    1235 : {
+        'fname' : "rishin",
+        'lname' : "kandi",
+        'mname' : "reddy",
+        'uid' : 67891034567
+    },
+    1236 : {
+        'fname' :  "rishin",
+        'lname' : "kandi",
+        'name' : "reddy",
+        'uid' : 67891034567
+    }
+}
+
+print (dict1)
